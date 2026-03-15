70264ec4ef4df570856c3cb57603c74dcfa65e67
Luis Diego Montero Vargas <diegomonter58@gmail.com>
2026-03-14 22:10:14 -0600

    Se agregan metodos para limpieza de datos, almacenamiento del nuevo dataset y modificaciones interactivas en el main

--- src/Helpers/cargadorDatos.py
+++ src/Helpers/cargadorDatos.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 
 class cargadorDatos:
 
@@ -7,11 +8,22 @@ class cargadorDatos:
         self.df = None
 
     def cargarDatos(self):
-        self.df = pd.read_csv(self.ruta)
+        self.df = pd.read_csv(self.ruta, encoding="utf-8")
         print("**************************************\n")
         print("El dataset posee:", self.df.shape[0], "filas\n")
         print("Nombre de las columnas", self.df.columns)
         print("**************************************\n")
+
+    def resumenNulos(self):
+        nulos = self.df.isnull().sum()
+        porcentaje = (self.df.isnull().mean() * 100).round(2)
+
+        resumen = pd.DataFrame({
+            "valores_nulos": nulos,
+            "porcentaje_%": porcentaje
+        })
+
+        return resumen
 
     def porcentajeNulos(self):
         print("**************************************\n")
@@ -22,3 +34,30 @@ class cargadorDatos:
         print("**************************************\n")
         print("Valores nulos por cada variable\n")
         return self.df.isnull().sum()
+
+    def limpiarColumnas(self):
+        print(self.df.columns)
+
+        columnas_eliminar = [
+            "adult",
+            "backdrop_path",
+            "genre_ids",
+            "id",
+            "popularity",
+            "poster_path",
+            "video"
+        ]
+
+        self.df.drop(columns=columnas_eliminar, inplace=True)
+
+        print("**************************************\n")
+        print("Columnas actuales despues de la limpieza:")
+        print(self.df.columns)
+
+    def guardarDatos(self):
+        ruta_salida = "../data/processed/tmdb_limpio.csv"
+
+        self.df.to_csv(ruta_salida, index=False)
+
+        print("\nDataset limpio guardado en:")
+        print(ruta_salida)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
+import time
 from src.helpers.cargadorDatos import cargadorDatos
 
 ruta = "../data/raw/tmdb_2020_to_2025.csv"
 
 cargador = cargadorDatos(ruta)
 
+print("\nCargando dataset...")
+time.sleep(5)
 cargador.cargarDatos()
 
-print(cargador.valoresNulos())
-print(cargador.porcentajeNulos())
+print("\nCalculando valores nulos...")
+time.sleep(6)
+print(cargador.resumenNulos())
+
+print("\nLimpiando columnas innecesarias...")
+time.sleep(6)
+cargador.limpiarColumnas()
+
+print("\nGuardando dataset limpio...")
+time.sleep(6)
+cargador.guardarDatos()
